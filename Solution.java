97c66a321a2b10f5f936f87f63d47f1c58da5ad7
ZJ <zzha9494@uni.sydney.edu.au>
2022-11-29 19:08:41 +1100

    q2

--- Solution.java
+++ Solution.java
@@ -1,5 +1,22 @@
 import java.util.HashMap;
 
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class Solution {
     // Q1
     public int[] twoSum(int[] nums, int target) {
@@ -10,5 +27,32 @@ public class Solution {
             map.put(nums[i], i);
         }
         return null;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // Q2
+        ListNode dummy = new ListNode();
+        ListNode cursor = dummy;
+        int carryFlag = 0, x, y;
+
+        while (l1 != null || l2 != null || carryFlag != 0) {
+            if (l1 != null) {
+                x = l1.val;
+                l1 = l1.next;
+            } else
+                x = 0;
+
+            if (l2 != null) {
+                y = l2.val;
+                l2 = l2.next;
+            } else
+                y = 0;
+
+            cursor.next = new ListNode((x + y + carryFlag) % 10);
+            cursor = cursor.next;
+            carryFlag = (x + y + carryFlag) / 10;
+        }
+
+        return dummy.next;
     }
 }
--- Solution.py
+++ Solution.py
@@ -1,4 +1,10 @@
-from typing import List
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
@@ -15,3 +21,24 @@ class Solution:
         for i in range(len(nums)):
             if target - nums[i] in nums[i + 1:]:
                 return [i, nums.index(target - nums[i], i + 1)]
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        """Q2"""
+        dummy = ListNode()
+        cursor = dummy
+        carry_flag = 0
+
+        while l1 or l2 or carry_flag:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            cursor.next = ListNode((x + y + carry_flag) % 10)
+            carry_flag = (x + y + carry_flag) // 10
+
+            cursor = cursor.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
