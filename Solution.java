422821272e6580ff74ac34c044ee1574557f88e2
ZJ <zzha9494@uni.sydney.edu.au>
2022-12-01 19:00:50 +1100

    q12

--- Solution.java
+++ Solution.java
@@ -176,6 +176,24 @@ public class Solution {
         return ans;
     }
 
+    // Q12
+    public String intToRoman(int num) {
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                ans.append(symbols[i]);
+                num -= values[i];
+
+                if (num == 0)
+                    break;
+            }
+        }
+        return ans.toString();
+    }
+
     public static void main(String[] args) {
         // Solution test = new Solution();
 
