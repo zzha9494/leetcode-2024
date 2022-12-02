7314595db4430b3cfddb2e511966482c197557c4
ZJ <zzha9494@uni.sydney.edu.au>
2022-12-02 16:53:10 +1100

    q15

--- Solution.java
+++ Solution.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class ListNode {
@@ -229,6 +232,39 @@ public class Solution {
             }
         }
         return strs[0];
+    }
+
+    // Q15
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return ans;
+            }
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[j++], nums[k--])));
+
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
