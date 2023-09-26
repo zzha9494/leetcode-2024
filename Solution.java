be4099369b76e9516733828f7b3c026a90efe41e
ZJ <zzha9494@uni.sydney.edu.au>
2023-09-26 21:28:37 +1000

    q73

--- Solution.java
+++ Solution.java
@@ -1314,6 +1314,48 @@ public class Solution {
         return dp[ch2.length];
     }
 
+    // Q73
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean column = false, row = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                column = true;
+                break;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                row = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (column) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (row) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
     // ------------------------------------------------------------------------
 
     public static void main(String[] args) {
--- Solution.py
+++ Solution.py
@@ -1250,6 +1250,30 @@ class Solution:
                 ), dp[j]
         return dp[-1]
 
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """Q73"""
+        m, n = len(matrix), len(matrix[0])
+        flag_column = any(matrix[i][0] == 0 for i in range(m))
+        flag_row = any(matrix[0][j] == 0 for j in range(n))
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = matrix[0][j] = 0
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+        if flag_column:
+            for i in range(m):
+                matrix[i][0] = 0
+
+        if flag_row:
+            for j in range(n):
+                matrix[0][j] = 0
+
 
 # ----------------------------------------------------
 s = Solution()
