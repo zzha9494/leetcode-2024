020a2b5e7c413cf76604810d2e7f006261e4e79e
ZJ <zzha9494@uni.sydney.edu.au>
2024-04-04 19:52:38 +1100

    Q139

--- Solution.py
+++ Solution.py
@@ -2387,10 +2387,25 @@ class Solution:
 
         return copyRandomList_helper(head)
 
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        """Q139"""
+        word_set = set(wordDict)
+        n = len(s)
+        # dp[i] 表示字符串s前i个字符组成的字符串 s[:i]
+        dp = [False] * (n+1)
+        for i in range(1, n+1):
+            if s[:i] in word_set:
+                dp[i] = True
+                continue
+            for j in range(1, i):
+                if dp[j] and s[j: i] in word_set:
+                    dp[i] = True
+                    break
+        return dp[-1]
+
 
 # ----------------------------------------------------
 s = Solution()
-gas = [1, 2, 3, 4, 5]
-cost = [3, 4, 5, 1, 2]
-a = s.canCompleteCircuit(gas, cost)
+
+a = s.wordBreak("leetcode", ["leet", "code"])
 print(a)
