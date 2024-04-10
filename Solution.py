3c87c9d284467c417a0bc1626ac06039e06d2e6e
ZJ <zzha9494@uni.sydney.edu.au>
2024-04-11 01:04:17 +1000

    Q155

--- Solution.py
+++ Solution.py
@@ -90,6 +90,30 @@ class LRUCache:
         return node
 
 
+class MinStack:
+    """Q155"""
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.getMin():
+            self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """Q1"""
