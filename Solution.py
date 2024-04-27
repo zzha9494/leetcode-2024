0a1990afe3deb16c5eade54cf90448b3dbe52c8e
ZJ <zzha9494@uni.sydney.edu.au>
2024-04-27 18:37:28 +1000

    Q202

--- Solution.py
+++ Solution.py
@@ -2962,6 +2962,21 @@ class Solution:
             right &= right - 1
         return right
 
+    def isHappy(self, n: int) -> bool:
+        """Q202"""
+        def next(n):
+            total = 0
+            while n:
+                n, digit = divmod(n, 10)
+                total += digit ** 2
+            return total
+
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = next(n)
+        return n == 1
+
 
 # ----------------------------------------------------
 s = Solution()
