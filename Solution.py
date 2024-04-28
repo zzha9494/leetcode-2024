219dcaf30456c981689c283067c6eaeb4db9b021
ZJ <zzha9494@uni.sydney.edu.au>
2024-04-28 20:23:53 +1000

    Q205

--- Solution.py
+++ Solution.py
@@ -2995,6 +2995,16 @@ class Solution:
                     isPrime[j] = 0
         return ans
 
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        """Q205"""
+        s2t, t2s = {}, {}
+        for i in range(len(s)):
+            if (s[i] in s2t and s2t[s[i]] != t[i]) or (t[i] in t2s and t2s[t[i]] != s[i]):
+                return False
+            s2t[s[i]] = t[i]
+            t2s[t[i]] = s[i]
+        return True
+
 
 # ----------------------------------------------------
 s = Solution()
